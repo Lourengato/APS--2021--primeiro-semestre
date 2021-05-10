e6a46afc36449aefe6018e5c8c62192d9462cb03
Lourengato <lourengato@gmail.com>
2021-05-09 22:25:43 -0300

    layout and style

--- APS-2021-1semestre/src/layouts/MainLayout.vue
+++ APS-2021-1semestre/src/layouts/MainLayout.vue
@@ -1,18 +1,13 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-     <q-header reveal class="bg-teal">
-        <q-toolbar>
-          <q-toolbar-title>Informações Corona Vírus</q-toolbar-title>
-        </q-toolbar>
-      </q-header>
-    <q-page-container>
-      <q-page-sticky position="top" expand class="bg-teal text-white">
-          <q-toolbar>
-            <q-btn flat round dense icon="map" />
-            <q-toolbar-title>Title</q-toolbar-title>
-          </q-toolbar>
-        </q-page-sticky>
+  <q-layout view="lHh Lpr lFf" class="bg-teal-1">
+    <q-page-container style="padding-top: 60px">
       <router-view />
+      <q-page-sticky position="top" expand class="bg-teal text-white">
+        <q-toolbar>
+            <q-btn flat round dense icon="medical_services" />
+              <q-toolbar-title>Informações Covid-19</q-toolbar-title>
+            </q-toolbar>
+        </q-page-sticky>
     </q-page-container>
   </q-layout>
 </template>
--- APS-2021-1semestre/src/pages/Index.vue
+++ APS-2021-1semestre/src/pages/Index.vue
@@ -1,11 +1,31 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
     <div class="q-pa-md">
+      <q-img
+      src="../assets/header.jpg"
+      spinner-color="white"
+      height="200px"
+      class="q-mb-sm"
+    />
       <q-table
         :data="info"
         :columns="columns"
+        :dense="$q.screen.lt.md"
         row-key="name"
+        table-header-class="text-teal"
+        :filter="filter"
+        hide-bottom
+        :pagination="{rowsPerPage: 0}"
       >
+        <template v-slot:top>
+          <div class="fit justify-center">
+            <q-input borderless dense debounce="300" outlined  v-model="filter" placeholder="Pesquisar">
+              <template v-slot:append>
+                <q-icon name="search" />
+              </template>
+            </q-input>
+          </div>
+        </template>
       </q-table>
     </div>
   </q-page>
@@ -18,9 +38,10 @@ export default {
 
    data () {
     return {
+      filter: '',
       info: data,
       columns: [
-        { name: 'local', align: 'left', label: 'Local', field: 'local', sortable: true },
+        { name: 'local', align: 'left', label: 'Local', field: 'local', sortable: true, headerClasses: 'bg-grey-2', classes: 'bg-grey-2 ellipsis', },
         { name: 'casos', align: 'left', label: 'Casos', field: 'casos', sortable: true },
         { name: 'novos_casos', align: 'left', label: 'Novos Casos', field: 'novos_casos', sortable: true },
         { name: 'casos_por_milhao', align: 'left', label: 'Casos por Milhão', field: 'casos_por_milhao', sortable: true  },
