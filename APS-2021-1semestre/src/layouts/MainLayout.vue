ac3380b2cec51051815c46573aa248150ed55a8e
Lourengato <lourengato@gmail.com>
2021-05-09 21:25:54 -0300

    quasar and table

---
+++ APS-2021-1semestre/src/layouts/MainLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+     <q-header reveal class="bg-teal">
+        <q-toolbar>
+          <q-toolbar-title>Informações Corona Vírus</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+    <q-page-container>
+      <q-page-sticky position="top" expand class="bg-teal text-white">
+          <q-toolbar>
+            <q-btn flat round dense icon="map" />
+            <q-toolbar-title>Title</q-toolbar-title>
+          </q-toolbar>
+        </q-page-sticky>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
---
+++ APS-2021-1semestre/src/pages/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md">
+      <q-table
+        :data="info"
+        :columns="columns"
+        row-key="name"
+      >
+      </q-table>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import data from '../../data.json';
+export default {
+  name: 'PageIndex',
+
+   data () {
+    return {
+      info: data,
+      columns: [
+        { name: 'local', align: 'left', label: 'Local', field: 'local', sortable: true },
+        { name: 'casos', align: 'left', label: 'Casos', field: 'casos', sortable: true },
+        { name: 'novos_casos', align: 'left', label: 'Novos Casos', field: 'novos_casos', sortable: true },
+        { name: 'casos_por_milhao', align: 'left', label: 'Casos por Milhão', field: 'casos_por_milhao', sortable: true  },
+        { name: 'mortes', align: 'left', label: 'Mortes', field: 'mortes', sortable: true  },
+      ],
+    }
+  },
+
+  methods: {
+  }
+}
+</script>
